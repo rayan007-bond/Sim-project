2ee4f29ba8704009526be4b8b23527a69cf84df5
Muhammad Rayan <129910583+rayan007-bond@users.noreply.github.com>
2025-09-14 17:11:27 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -65,24 +65,23 @@
 	var carousel = function() {
 		$('.carousel-testimony').owlCarousel({
 			center: true,
-			loop: false,
+			loop: true,                 // 🔥 must be true for autoplay
 			items:1,
 			margin: 30,
 			stagePadding: 0,
+			autoplay:true,              // 🔥 enable autoplay
+			autoplayTimeout:3000,       // 3 sec delay
+			autoplayHoverPause:true,    // pause when hovered
 			nav: false,
+			dots:true,                  // show dots
 			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
 			responsive:{
-				0:{
-					items: 1
-				},
-				600:{
-					items: 2
-				},
-				1000:{
-					items: 3
-				}
+				0:{ items: 1 },
+				600:{ items: 2 },
+				1000:{ items: 3 }
 			}
 		});
+
 
 		$('.carousel-case').owlCarousel({
 			center: true,
